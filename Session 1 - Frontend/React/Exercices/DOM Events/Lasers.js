afebbaed5e0cd8c1d431aa74f8443fa5549caed3
henriChevreux <henri.chevreux1@gmail.com>
2024-01-26 20:59:04 +0000

    Upload LAsers component

---
+++ Session 1 - Frontend/React/Exercices/DOM Events/Lasers.js
@@ -0,0 +1,19 @@
+
+
+function Lasers() {
+    function activateLasers() {
+        console.log("pwew pwew");
+    }
+
+    return(
+        <button onClick={activateLasers}>
+            Activate Lasers
+        </button>
+    )
+
+
+}
+
+export default Lasers;
+
+
